fb6eb298ea3480a51d70f2b8b5e659b23352fec1
Tiff Chan <tiffchan@Tiffanys-MacBook-Air.local>
2023-12-10 22:22:15 -0500

    Removed unecessary step in svg generator

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// Importing libraries that are needed
 const inquirer = require("inquirer");
-
 const fs = require("fs");
-
 const shape = require("./lib/shapes.js");
+
+// Helper function to create logo
 
 function createShape(fileName, shape, text) {
     const svg = 
@@ -14,6 +15,8 @@ function createShape(fileName, shape, text) {
     fs.writeFile(fileName, svg, (error) =>
     error ? console.error(error) : console.log(shape))
 }
+
+// Prompting for user input
 
 inquirer
     .prompt ([
@@ -39,33 +42,25 @@ inquirer
             name: "shapeColor",
         },
     ])
+    // Generating the SVG
     .then((responses) => {
-        fs.writeFile("shape-information.txt", JSON.stringify(responses), (error) =>
-        error ? console.error(error) : console.log(responses))
-    })
-    .then(() => {
-        fs.readFile('shape-information.txt', function read(err, data) {
-            if (err) {
-                throw err;
-            }
-            const shapeInfo = JSON.parse(data);
-        
-            if (shapeInfo.shape[0] === "Circle") {
-                const circle = new shape.Circle(100, 100, shapeInfo.text, shapeInfo.textColor, shapeInfo.shapeColor);
-                const shapeSVG = circle.shapeCreate();
-                const shapeText = circle.textCreate();
-                createShape("shape.html", shapeSVG, shapeText);
-            } else if (shapeInfo.shape[0] === "Square") {
-                const square = new shape.Square(0, 0, shapeInfo.text, shapeInfo.textColor, shapeInfo.shapeColor);
-                const shapeSVG = square.shapeCreate();
-                const shapeText = square.textCreate();
-                createShape("shape.html", shapeSVG, shapeText);
-            } else if (shapeInfo.shape[0] === "Triangle") {
-                const triangle = new shape.Triangle(0, 0, shapeInfo.text, shapeInfo.textColor, shapeInfo.shapeColor);
-                const shapeSVG = triangle.shapeCreate();
-                const shapeText = triangle.textCreate();
-                createShape("shape.html", shapeSVG, shapeText);
-            }
-        });
+        const shapeInfo = responses;
+    
+        if (shapeInfo.shape[0] === "Circle") {
+            const circle = new shape.Circle(100, 100, shapeInfo.text, shapeInfo.textColor, shapeInfo.shapeColor);
+            const shapeSVG = circle.shapeCreate();
+            const shapeText = circle.textCreate();
+            createShape("circle.svg", shapeSVG, shapeText);
+        } else if (shapeInfo.shape[0] === "Square") {
+            const square = new shape.Square(0, 0, shapeInfo.text, shapeInfo.textColor, shapeInfo.shapeColor);
+            const shapeSVG = square.shapeCreate();
+            const shapeText = square.textCreate();
+            createShape("square.svg", shapeSVG, shapeText);
+        } else if (shapeInfo.shape[0] === "Triangle") {
+            const triangle = new shape.Triangle(0, 0, shapeInfo.text, shapeInfo.textColor, shapeInfo.shapeColor);
+            const shapeSVG = triangle.shapeCreate();
+            const shapeText = triangle.textCreate();
+            createShape("triangle.svg", shapeSVG, shapeText);
+        }
     });
 
